94a315413dfe074a46b4bc60e210d882f45cbd68
TeaDrivenDev <TeaDrivenDev@gmail.com>
2016-06-10 03:20:20 +0200

    Minimal implementation

--- GjallarhornTest/Library1.fs
+++ GjallarhornTest/Library1.fs
@@ -1,4 +1,55 @@
 ﻿namespace GjallarhornTest
 
-type Class1() = 
-    member this.X = "F#"
+open System
+open System.Windows.Forms
+
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Observable
+open Gjallarhorn.Validation
+
+type ModelThing = { Directory : string }
+
+module VM =
+
+    let getDirectory() =
+        use dialog = new FolderBrowserDialog()
+        match dialog.ShowDialog() with
+        | DialogResult.OK -> Some dialog.SelectedPath
+        | _ -> None
+
+    let create (thingIn : IObservable<ModelThing>) initialValue =
+        let subject = Binding.createSubject()
+
+        let source = subject.ObservableToSignal initialValue thingIn
+
+        let directory = source |> Binding.editMember subject <@ initialValue.Directory @> noValidation
+
+        let chooseDirectoryCommand = subject.Command "ChooseDirectoryCommand"
+
+        subject
+
+module Model =
+    
+    let directoryHistory = Mutable.create []
+
+    let directoryStream = Observable.map List.head directoryHistory
+
+    let add directory =
+        directoryHistory
+        |> Mutable.step (fun previous -> directory :: previous)
+
+
+module Program =
+
+    [<STAThread>]
+    [<EntryPoint>]
+    let main _ =
+        Gjallarhorn.Wpf.install true |> ignore
+
+        let app = System.Windows.Application()
+
+        let window = GjallarhornTest.UI.MainView()
+        window.DataContext <- VM.create Model.directoryStream { Directory = @"C:\" }
+
+        app.Run window
