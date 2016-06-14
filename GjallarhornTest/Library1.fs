bf1bdd857bf2af32276ed15afb86724ac78bfa07
ReedCopsey <reed@reedcopsey.com>
2016-06-14 11:32:26 -0700

    Cleanup of GjallarhornTest

--- GjallarhornTest/Library1.fs
+++ GjallarhornTest/Library1.fs
@@ -11,8 +11,6 @@ open Gjallarhorn.Validation
 type ModelThing = { Directory : string }
 
 module VM =
-    let noValidation _ = None
-
     let directoryExists dir = if System.IO.Directory.Exists dir then None else Some "No"
 
     let getDirectory() =
@@ -21,23 +19,22 @@ module VM =
         | DialogResult.OK -> Some dialog.SelectedPath
         | _ -> None
 
-    let create (thingIn : IObservable<ModelThing>) initialValue =
-        let bindingSource = Binding.createObservableSource()
+    let create (source : ISignal<ModelThing>) =
+        let bindingSource = Binding.createObservableSource()        
 
-        let source = bindingSource.ObservableToSignal initialValue thingIn
+        let directory = source |> Binding.memberToFromView bindingSource <@ source.Value.Directory @> (Validation.custom directoryExists)
 
-        let directory = source |> Binding.memberToFromView bindingSource <@ initialValue.Directory @> (Validation.custom directoryExists)
-
-        let dir = Signal.map (fun d -> { Directory = d }) directory
+        let dir = 
+            directory
+            |> Signal.map (fun d -> { Directory = d }) 
+            |> bindingSource.FilterValid // Note that this is required to prevent "invalid" results from showing in the stream
 
         let chooseDirectoryCommand = bindingSource |> Binding.createCommand "ChooseDirectoryCommand"
 
         let chooseDir =
             chooseDirectoryCommand
-            |> Observable.map (fun _ ->
-                match getDirectory() with
-                | Some dir -> { source.Value with Directory = dir }
-                | None -> source.Value)
+            |> Observable.choose (fun _ -> getDirectory())
+            |> Observable.map (fun d -> { Directory = d })
 
         dir
         |> Observable.merge chooseDir
@@ -47,13 +44,15 @@ module VM =
 
 module Model =
     
-    let directoryHistory = Mutable.create []
+    let directoryHistory = Mutable.create [ { Directory = @"C:\" } ]
 
-    let directoryStream = Observable.map List.head directoryHistory
+    let directoryStream = Signal.map List.head directoryHistory
 
     let add directory =
         directoryHistory
         |> Mutable.step (fun previous -> directory :: previous)
+
+    let _subscription = directoryHistory.Subscribe (List.head >> printfn "New: %A")
 
 
 module Program =
@@ -65,7 +64,7 @@ module Program =
 
         let app = System.Windows.Application()
 
-        let vm = VM.create Model.directoryStream { Directory = @"C:\" }
+        let vm = VM.create Model.directoryStream 
 
         let window = GjallarhornTest.UI.MainView()
         window.DataContext <- vm
